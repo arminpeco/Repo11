93de93a113703e2cd5589aae51aa1f0ada927dec
arminpeco <apeco1906@gmail.com>
2021-09-24 01:19:28 +0200

    Added file index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+
+
+  
+    // Only change code below this line
+    var myNestedArray=[["Drina",1],["Cips",1]];
+
+    function myNestedFunction(arr){
+
+       arr.push(["Toblerone",5],["Milka",3])
+       return arr;
+          
+}
+    // Only change code below this line
+  
+
+console.log(myNestedFunction(myNestedArray)) // Change this line
+module.exports=myNestedFunction;
